c2f748321471a22dede4e6fec548c6f3716f5b6f
Head Gardener <trashbin2019np@gmail.com>
2024-02-06 07:46:36 +0300

    easyeffects shared mod

---
+++ modules/share/easyeffects/default.nix
@@ -0,0 +1,45 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+
+  cfg = config.services.easyeffects;
+
+in {
+
+  options.services.easyeffects = {
+    enable = mkEnableOption "easyeffects";
+
+    package = mkPackageOptionMD pkgs "easyeffects" { };
+
+    args = mkOption {
+      type = types.listOf types.str;
+      default = [];
+      example = [
+        "--load-preset music"
+        "--bypass 2"
+      ];
+      description = lib.mdDoc ''
+        Arguments passed to easyeffects binary.
+      '';
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.user.services.easyeffects = {
+      description = "EasyEffects";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+
+      serviceConfig = {
+        ExecStart = "${getExe cfg.package} ${concatStringsSep " " cfg.args}";
+        RestartSec = 3;
+        Restart = "always";
+      };
+    };
+
+    environment.systemPackages = [ cfg.package ];
+  };
+
+}
