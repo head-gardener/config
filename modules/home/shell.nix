d05918b508c9cde4b196c426bc388a57fc6520e1
Head Gardener <trashbin2019np@gmail.com>
2024-02-13 21:58:28 +0300

    starship: initial settings

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -14,9 +14,34 @@
       enable = true;
       settings = {
         add_newline = true;
+        format = ''
+          $username$hostname$directory$git_branch$git_state$git_status$cmd_duration$nix_shell
+          $character
+        '';
         character = {
-          success_symbol = "[➜](bold green)";
-          error_symbol = "[➜](bold red)";
+          success_symbol = "[❯](cyan)";
+          error_symbol = "[❯](red)";
+          vimcmd_symbol = "[❮](green)";
+        };
+        directory.style = "blue";
+        git_branch.format = "[$branch](bright-black)";
+        git_status = {
+          format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+          style = "cyan";
+          conflicted = "​";
+          untracked = "​";
+          modified = "​";
+          staged = "​";
+          renamed = "​";
+          deleted = "​";
+          stashed = "≡";
+        };
+        nix_shell = {
+          disabled = false;
+          impure_msg = "[impure shell](bold red)";
+          pure_msg = "[pure shell](bold green)";
+          unknown_msg = "[unknown shell](bold yellow)";
+          format = "in [☃️ $state( \($name\))](bold blue) ";
         };
       };
     };
