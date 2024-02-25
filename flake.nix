62f114203e22ee7cefe14fe5bded09f520ca65ac
Head Gardener <trashbin2019np@gmail.com>
2024-02-25 19:04:38 +0300

    refactor: le grand flake-parts rewrite

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
     blog.url = "github:head-gardener/blog";
     # blog.url = "/home/hunter/Blog/";
     dmenu-conf.url = "github:head-gardener/nixpkgs/master";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
     hydra.url = "github:NixOS/hydra";
     musnix.url = "github:musnix/musnix";
@@ -27,33 +27,116 @@
     # xmonad.url = "/home/hunter/xmonad/";
   };
 
-  outputs = inputs: with inputs; {
+  outputs = inputs: with inputs; flake-parts.lib.mkFlake { inherit inputs; } {
+    flake = {
+      lib = import ./lib.nix inputs nixpkgs.lib;
 
-    lib = import ./lib.nix inputs nixpkgs.lib;
+      overlays = self.lib.mkOverlays inputs ./overlays // {
+        packages = final: _: import ./pkgs final;
+      };
 
-    overlays = self.lib.mkOverlays inputs ./overlays // {
-      packages = final: prev: import ./pkgs final;
+      nixosModules =
+        let
+          share = self.lib.genAttrsFromDir ./modules/share import;
+        in
+        {
+          home = { inputs, ... }: {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.hunter = import ./modules/home.nix;
+              extraSpecialArgs = {
+                inherit inputs;
+                system = "x86_64-linux";
+              };
+            };
+          };
+
+          default.imports =
+            (self.lib.ls ./modules/default) ++ [
+              { nixpkgs.overlays = nixpkgs.lib.attrValues self.overlays; }
+            ] ++ nixpkgs.lib.attrValues share;
+
+          desktop.imports = [
+            { nixpkgs.overlays = [ neovim-nightly.overlay ]; }
+            ./modules/doas.nix
+            ./modules/lightdm.nix
+            ./modules/picom.nix
+            ./modules/pipewire.nix
+            ./modules/xserver.nix
+          ];
+        } // share;
+
+      nixosConfigurations = {
+        distortion = self.lib.mkDesktop "x86_64-linux" "distortion" [
+          musnix.nixosModules.musnix
+          {
+            musnix.enable = true;
+            users.users.hunter.extraGroups = [ "audio" ];
+          }
+          ./modules/cache.nix
+          ./modules/nvidia.nix
+          ./modules/xmonad.nix
+          ./modules/github.nix
+          ({ pkgs, ... }: { environment.binsh = "${pkgs.dash}/bin/dash"; })
+        ];
+
+        shears = self.lib.mkDesktop "x86_64-linux" "shears" [
+          (self.lib.mkKeys self "hunter")
+          xmonad.nixosModules.powerwatch
+          ./modules/cache.nix
+          ./modules/github.nix
+          ./modules/xmonad.nix
+          ./modules/upower.nix
+          { services.easyeffects.enable = true; }
+          { boot.kernel.sysctl = { "vm.swappiness" = 20; }; }
+        ];
+
+        apple = self.lib.mkHost "x86_64-linux" "apple" [
+          (self.lib.mkKeys self "hunter")
+          ./modules/cache.nix
+        ];
+
+        blueberry = self.lib.mkHost "x86_64-linux" "blueberry" [
+          ./modules/nginx.nix
+          ./modules/nas.nix
+          ./modules/hydra.nix
+          blog.nixosModules.blog
+          (self.lib.mkKeys self "hunter")
+          agenix.nixosModules.default
+          ./modules/refresher-staging.nix
+          ./modules/refresher-config.nix
+        ];
+      };
     };
 
-    checks = {
-      inherit (self.hydraJobs) x86_64-linux;
-    };
+    systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
 
-    hydraJobs.x86_64-linux =
-      { inherit (self.packages) x86_64-linux; }.x86_64-linux // {
+    perSystem = { pkgs, system, self', ... }: {
+      # TODO: doesn't work!
+      # _module.args.pkgs = import inputs.nixpkgs {
+      #   inherit system;
+      #   overlays = [
+      #     self.overlays.packages
+      #   ];
+      # };
+      # packages = import ./pkgs pkgs;
+
+      packages = import ./pkgs (self.lib.nixpkgsFor system);
+
+      checks = self'.packages // {
         mkHost = nixpkgs.lib.nixos.runTest {
           name = "mkHost-test";
 
           node.specialArgs = {
             inherit inputs;
-            system = "x86_64-linux";
           };
 
           nodes.machine = {
             imports = self.lib.mkHostModules "test";
           };
 
-          hostPkgs = nixpkgs.legacyPackages.x86_64-linux;
+          hostPkgs = pkgs;
 
           testScript = ''
             start_all()
@@ -70,7 +153,6 @@
 
           node.specialArgs = {
             inherit inputs;
-            system = "x86_64-linux";
           };
 
           nodes.machine = {
@@ -80,7 +162,7 @@
             home-manager.users.hunter.manual.manpages.enable = false;
           };
 
-          hostPkgs = nixpkgs.legacyPackages.x86_64-linux;
+          hostPkgs = pkgs;
 
           testScript = ''
             start_all()
@@ -93,88 +175,6 @@
           '';
         };
       };
-    # takes too long to evaluate
-    #// {
-    #  x86_64-linux = nixpkgs.lib.mapAttrs
-    #    (n: v: v.config.system.build.toplevel)
-    #    self.nixosConfigurations;
-    #};
-
-    nixosModules =
-      let
-        share = self.lib.genAttrsFromDir ./modules/share import;
-      in
-      {
-        home = { inputs, ... }: {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            users.hunter = import ./modules/home.nix;
-            extraSpecialArgs = {
-              inherit inputs;
-              system = "x86_64-linux";
-            };
-          };
-        };
-
-        default.imports =
-          (self.lib.ls ./modules/default) ++ [
-            { nixpkgs.overlays = nixpkgs.lib.attrValues self.overlays; }
-          ] ++ nixpkgs.lib.attrValues share;
-
-        desktop.imports = [
-          { nixpkgs.overlays = [ neovim-nightly.overlay ]; }
-          ./modules/doas.nix
-          ./modules/lightdm.nix
-          ./modules/picom.nix
-          ./modules/pipewire.nix
-          ./modules/xserver.nix
-        ];
-      } // share;
-
-    nixosConfigurations = {
-      distortion = self.lib.mkDesktop "x86_64-linux" "distortion" [
-        musnix.nixosModules.musnix
-        {
-          musnix.enable = true;
-          users.users.hunter.extraGroups = [ "audio" ];
-        }
-        ./modules/cache.nix
-        ./modules/nvidia.nix
-        ./modules/xmonad.nix
-        ./modules/github.nix
-        ({ pkgs, ... }: { environment.binsh = "${pkgs.dash}/bin/dash"; })
-      ];
-
-      shears = self.lib.mkDesktop "x86_64-linux" "shears" [
-        (self.lib.mkKeys self "hunter")
-        xmonad.nixosModules.powerwatch
-        ./modules/cache.nix
-        ./modules/github.nix
-        ./modules/xmonad.nix
-        ./modules/upower.nix
-        { services.easyeffects.enable = true; }
-        { boot.kernel.sysctl = { "vm.swappiness" = 20; }; }
-      ];
-
-      apple = self.lib.mkHost "x86_64-linux" "apple" [
-        (self.lib.mkKeys self "hunter")
-        ./modules/cache.nix
-      ];
-
-      blueberry = self.lib.mkHost "x86_64-linux" "blueberry" [
-        ./modules/nginx.nix
-        ./modules/nas.nix
-        ./modules/hydra.nix
-        blog.nixosModules.blog
-        (self.lib.mkKeys self "hunter")
-        agenix.nixosModules.default
-        ./modules/refresher-staging.nix
-        ./modules/refresher-config.nix
-      ];
     };
-
-  } // (flake-utils.lib.eachDefaultSystem (system: {
-    packages = import ./pkgs (self.lib.nixpkgsFor system);
-  }));
+  };
 }
