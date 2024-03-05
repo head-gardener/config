e72bc2f263bb85e40c9211708a161be4f3a8ad03
Head Gardener <trashbin2019np@gmail.com>
2024-03-05 21:01:58 +0300

    feat(fish): some aliases

--- modules/home/fish.nix
+++ modules/home/fish.nix
@@ -13,6 +13,8 @@ in
       la = "ls -al";
       ll = "ls -l";
       mux = "tmuxinator";
+      tp = "pushd $(mktemp -d)";
+      mux-tp = "tmux new-session -d -c $(mktemp -d) -s tmp";
     };
     shellAbbrs = {
       loc = "XDG_CONFIG_HOME=~/config/dots";
@@ -26,6 +28,16 @@ in
     interactiveShellInit = ''
       ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right | source
       setenv LS_COLORS "${builtins.readFile pkgs.ls_colors}"
+
+      function nom-rebuild-d --wraps nixos-rebuild --description "nom-wrapped doas nixos-rebuild"
+        doas w > /dev/null
+        doas nixos-rebuild $argv --log-format internal-json -v 2>| nom --json
+      end
+
+      function nom-rebuild --wraps nixos-rebuild --description "nom-wrapped nixos-rebuild"
+        nixos-rebuild $argv --log-format internal-json -v 2>| nom --json
+      end
+
     '';
     plugins = map nixplug (with pkgs.fishPlugins; [
       abbreviation-tips
