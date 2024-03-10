5ac11970761806a6443a26b76c4ff582514bd951
Head Gardener <trashbin2019np@gmail.com>
2024-03-10 17:34:54 +0300

    fix(fish): motd colors

--- dots/fish/functions/fish_greeting.fish
+++ dots/fish/functions/fish_greeting.fish
@@ -2,6 +2,7 @@ function fish_greeting
   tput civis
 
   set sl 0.03
+  echo
   echo -ne $fish_motd | while read -l $line
     echo -e $line
     if test $sl -gt 0
@@ -9,9 +10,10 @@ function fish_greeting
     end
     sleep $sl
   end
+  echo
 
   set time $(uptime | sed -E 's|.*up\s+(.*),\s+[0-9]+\s+user.*|\1|g')
-  echo -e "$(set_color bryellow )| no sleep for $(set_color red)$time$(set_color bryellow) and counting |$(set_color normal)"
+  echo -e "$(set_color blue)| no sleep for $(set_color cyan)$time$(set_color blue) and counting |$(set_color normal)"
 
   tput cnorm
 end
