588dce43c6524292f63b9fd3119f5daee0c23b47
head-gardener <49650767+head-gardener@users.noreply.github.com>
2024-03-18 17:10:22 +0300

    fix(lilex): updates src (#7)

--- pkgs/fonts/lilex.nix
+++ pkgs/fonts/lilex.nix
@@ -8,19 +8,18 @@ stdenv.mkDerivation {
     (fetchFromGitHub {
       owner = "mishamyrt";
       repo = "Lilex";
-      rev = "0bb53c1458f3a511feb1ced97b5682c014b02397";
-      hash = "sha256-xanRGw6jTdvy0agZAzUCqh9NeP9Sji+temUPtPr+KSc=";
+      rev = "0c63d0d919201078c6b8f2364c295a3c034da503";
+      hash = "sha256-WJcLhrEpl4Qy+Mhw7Sajh6GO1GpTCVB6bjnpjxlFnSI= ";
     })
   ];
 
-  outputHash = "sha256-+3qVLFZ8OAb00pnajo0Fh6UQZ1H0jDlcxp0txauB3vY=";
+  outputHash = "sha256-06G/WVskq3RihPfTa6reL31HUu8EayMcIjjJ/mzDx8o=";
   outputHashAlgo = "sha256";
   outputHashMode = "recursive";
 
   configurePhase = ''
     python -m venv venv
     . venv/bin/activate
-    cp ${./requirements.txt} requirements.txt
     pip install --no-cache-dir -r requirements.txt
   '';
 
