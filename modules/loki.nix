a2858ba5044c6dacf306c2d823f4208e6fba7b48
Head Gardener <trashbin2019np@gmail.com>
2024-08-14 12:59:49 +0300

    chore(loki): schema v11 -> v13, tsdb

--- modules/loki.nix
+++ modules/loki.nix
@@ -22,15 +22,14 @@
         max_chunk_age = "1h";
         chunk_target_size = 999999;
         chunk_retain_period = "30s";
-        max_transfer_retries = 0;
       };
 
       schema_config = {
         configs = [{
           from = "2022-06-06";
-          store = "boltdb-shipper";
+          store = "tsdb";
           object_store = "filesystem";
-          schema = "v11";
+          schema = "v13";
           index = {
             prefix = "index_";
             period = "24h";
@@ -39,25 +38,20 @@
       };
 
       storage_config = {
-        boltdb_shipper = {
-          active_index_directory = "/var/lib/loki/boltdb-shipper-active";
-          cache_location = "/var/lib/loki/boltdb-shipper-cache";
-          cache_ttl = "24h";
-          shared_store = "filesystem";
-        };
-
         filesystem = {
           directory = "/var/lib/loki/chunks";
+        };
+
+        tsdb_shipper = {
+          active_index_directory = "/var/lib/loki/tsdb-shipper-active";
+          cache_location = "/var/lib/loki/tsdb-shipper-cache";
+          cache_ttl = "24h";
         };
       };
 
       limits_config = {
         reject_old_samples = true;
         reject_old_samples_max_age = "168h";
-      };
-
-      chunk_store_config = {
-        max_look_back_period = "0s";
       };
 
       table_manager = {
@@ -67,7 +61,6 @@
 
       compactor = {
         working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
         compactor_ring = {
           kvstore = {
             store = "inmemory";
