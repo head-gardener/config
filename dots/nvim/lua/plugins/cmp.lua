5a8be3c9934065085984df26fc33a26e7b9dc257
Head Gardener <trashbin2019np@gmail.com>
2024-08-20 05:07:31 +0300

    refactor(neovim): separates cmp from lsp

---
+++ dots/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,50 @@
+return {
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'VeryLazy',
+    init = function()
+      local cmp = require('cmp')
+
+      local feedkey = function(key, mode)
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+      end
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+          end,
+        },
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+        mapping = {
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+              feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            else
+              fallback()
+            end
+          end, { "i", "s", noremap = true }),
+          ['<S-Tab>'] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, { "i", "s" })
+        },
+      })
+    end,
+  },
+  'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-buffer',
+  'hrsh7th/cmp-path',
+  'hrsh7th/cmp-cmdline',
+}
