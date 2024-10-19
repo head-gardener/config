143b71c386beff0497f91083b3e6206cfae015ed
Head Gardener <trashbin2019np@gmail.com>
2024-10-19 10:43:47 +0300

    delete(neovim): iron

--- dots/nvim/lua/keymaps.lua
+++ dots/nvim/lua/keymaps.lua
@@ -20,29 +20,6 @@ vim.keymap.set('n', '<Leader>sb', ':set lbr!<CR>', { noremap = true })
 vim.keymap.set('n', '<Leader>sw', ':set wrap!<CR>', { noremap = true })
 vim.keymap.set('n', '<Leader>ss', ':set spell!<CR>', { noremap = true })
 
--- Terminal
-local iron = require('iron.core')
-vim.keymap.set('n', '<Space>ts', function() iron.repl_for('sh') end, { noremap = true })
-vim.keymap.set('n', '<Space>tb', function()
-  -- I should be banned from programming for this
-  local cfg = require('iron.config')
-  local f = cfg.repl_open_cmd
-  cfg.repl_open_cmd = "botright 20 split",
-      -- cfg should be read before using or cmd won't be set
-      -- or something idk, doesn't work without the assert.
-      assert(cfg.repl_open_cmd)
-  iron.repl_for('sh')
-  cfg.repl_open_cmd = f
-end, { noremap = true })
-vim.keymap.set('n', '<Space>tq', function() iron.close_repl('sh') end, { noremap = true })
-vim.keymap.set('n', '<Space>t<Space>', function() iron.send('sh', '\3') end, { noremap = true })
-vim.keymap.set('n', '<Space>tf', function()
-  iron.focus_on('sh')
-  vim.cmd('startinsert')
-end, { noremap = true })
-vim.keymap.set('n', '<Space>th', function() iron.hide_repl('sh') end, { noremap = true })
-vim.keymap.set('n', '<Space>tk', function() iron.send('sh', '\27[A') end, { noremap = true })
-
 -- Folds
 vim.keymap.set({ 'n', 'v' }, '<M-h>', '[z', { noremap = true })
 vim.keymap.set({ 'n', 'v' }, '<M-l>', ']z', { noremap = true })
--- dots/nvim/lua/plugins/ui.lua
+++ dots/nvim/lua/plugins/ui.lua
@@ -92,61 +92,6 @@ return {
   },
   { 'sindrets/diffview.nvim', lazy = false },
   {
-    'hkupty/iron.nvim',
-    config = function()
-      require('iron.core').setup {
-        config = {
-          scratch_repl = true,
-          repl_definition = {
-            sh = {
-              command = { 'fish' }
-            },
-            rust = {
-              command = { 'fish' }
-            },
-            haskell = {
-              command = { 'cabal', 'repl' }
-            },
-            nix = {
-              command = { 'nix', 'repl' }
-            }
-          },
-          repl_open_cmd = require('iron.view').split.vertical.topleft(50),
-        },
-        -- Iron doesn't set keymaps by default anymore.
-        -- You can set them here or manually add keymaps to the functions in iron.core
-        keymaps = {
-          send_motion = '<space>sc',
-          visual_send = '<space>sc',
-          send_file = '<space>sf',
-          send_line = '<space>sl',
-          send_mark = '<space>sm',
-          mark_motion = '<space>mc',
-          mark_visual = '<space>mv',
-          remove_mark = '<space>md',
-          cr = '<space>s<cr>',
-          interrupt = '<space>s<space>',
-          exit = '<space>sq',
-          clear = '<space>cr',
-        },
-        -- If the highlight is on, you can change how it looks
-        -- For the available options, check nvim_set_hl
-        highlight = {
-          italic = true
-        },
-        ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
-      }
-    end,
-    keys = function()
-      return {
-        { '<space>rs', '<cmd>IronRepl<cr>' },
-        { '<space>rr', '<cmd>IronRestart<cr>' },
-        { '<space>rf', '<cmd>IronFocus<cr>' },
-        { '<space>rh', '<cmd>IronHide<cr>' },
-      }
-    end,
-  },
-  {
     'folke/zen-mode.nvim',
     opts = {
       window = {
