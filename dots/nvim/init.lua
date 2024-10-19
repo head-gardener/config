6133622895e8aed653af26618427a4268b969ac5
Head Gardener <trashbin2019np@gmail.com>
2024-10-19 11:27:25 +0300

    feat(neovim): ; leader, \ localleader

--- dots/nvim/init.lua
+++ dots/nvim/init.lua
@@ -1,7 +1,9 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-vim.g.mapleader = "\\"
+vim.g.mapleader = ";"
+vim.g.maplocalleader = " "
+vim.keymap.set({ 'n', 'v' }, ';;', ';', { noremap = true })
 
 -- require('lazy-bootstrap')
 require('lazy').setup('plugins', {
