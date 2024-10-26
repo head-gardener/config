8caad3e9ce9e4f84d35b22863d6c346a9655ebb3
Head Gardener <trashbin2019np@gmail.com>
2024-10-27 01:15:57 +0300

    test: init

---
+++ test/flake.nix
@@ -0,0 +1,29 @@
+{
+  description =
+    "A flake containing everything that needs to be checked with nix flake check";
+
+  inputs.parent.url = "..";
+
+  outputs = inputs:
+    with inputs;
+    parent.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = {
+        nixosConfigurations = {
+          inherit (parent.nixosConfigurations)
+            ambrosia distortion blueberry elderberry;
+        };
+
+        overlays = parent.overlays;
+
+        nixosModules = parent.nixosModules;
+      };
+
+      systems = [ "x86_64-linux" ];
+
+      perSystem = { inputs', ... }: {
+        packages = inputs'.parent.packages // parent.images;
+
+        checks = inputs'.parent.checks;
+      };
+    };
+}
