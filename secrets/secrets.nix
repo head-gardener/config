d2efd799b5d06adfc7f6b8063ec0777c6817888f
Head Gardener <trashbin2019np@gmail.com>
2024-10-26 19:37:19 +0300

    feat(agenix): adds ambrosia to some secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,6 +3,7 @@ let
   hunter-shears = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDY4vNmrHf1QXxSByGp+HM5aYXIlCkpkNqPeTsRiWWOh";
   hunter-ambrosia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIiiSLosPqVfFJUepkajqhuNSxn3+jjTNOGjyTpKzXKv";
   users = [ hunter hunter-shears hunter-ambrosia ];
+  admins = [ hunter hunter-ambrosia ];
 
   distortion = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFeKWUEkTCsPQaXzoUc/5JF0BecasFaiSt/f/HavHJPo";
   blueberry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIELJrb6Drbgtrg3jRcvevq8kG1N03cji3k3FkmwUFxo+";
@@ -17,13 +18,13 @@ in
   "cache.age".publicKeys = [ hunter ] ++ deploy;
   "id_gh.age".publicKeys = [ hunter ] ++ systems;
   "id_conf.age".publicKeys = [ hunter ] ++ deploy;
-  "minio-creds.age".publicKeys = [ hunter ] ++ deploy;
+  "minio-creds.age".publicKeys = admins ++ deploy;
   "s3-hunter.age".publicKeys = users ++ systems;
   "s3-ro.age".publicKeys = users ++ systems;
-  "s3-backup.age".publicKeys = [ hunter ] ++ deploy;
+  "s3-backup.age".publicKeys = admins ++ deploy;
   "s3-torrent.age".publicKeys = [ hunter ] ++ deploy;
   "cherry-gpg.age".publicKeys = [ hunter cherry ];
   "blueberry-gpg.age".publicKeys = [ hunter blueberry ];
   "vmess-uuid.age".publicKeys = users ++ systems;
-  "jenkins-slave-secret.age".publicKeys = users ++ [ blueberry ];
+  "jenkins-slave-secret.age".publicKeys = admins ++ [ blueberry ];
 }
