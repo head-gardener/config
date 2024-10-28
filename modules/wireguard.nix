760a3ca2828bc96bbfa82d77c123e8cc9aba5e53
Head Gardener <trashbin2019np@gmail.com>
2024-10-28 19:59:30 +0300

    feat(wireguard): dns

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -6,64 +6,84 @@
         default = 43721;
       };
       isClient = lib.mkOption { type = lib.types.bool; };
+      interface = lib.mkOption {
+        type = lib.types.str;
+        default = "wg0";
+      };
     };
   };
 
   config = let
     cfg = config.services.wg;
     hosts = builtins.fromJSON (builtins.readFile "${inputs.self}/hosts.json");
+    server = hosts."${alloy.wireguard-server.hostname}";
+    hosts-file = pkgs.writeText "dnsmasq-wg-hosts"
+      (builtins.concatStringsSep "\n"
+        (map (h: "${h.value.ipv4} ${h.name}.wg") (lib.attrsToList hosts)));
+
   in {
     age.secrets.wg = {
       file = "${inputs.self}/secrets/wg/${config.networking.hostName}.age";
     };
 
+    environment.systemPackages = [ pkgs.wireguard-tools ];
+
     networking.wireguard.enable = true;
     networking.firewall.allowedUDPPorts = [ cfg.port ];
-
-    environment.systemPackages = [ pkgs.wireguard-tools ];
+    networking.firewall.trustedInterfaces = [ "wg0" ];
 
     networking.nat = lib.mkIf (!cfg.isClient) {
       enable = true;
       internalInterfaces = [ "wg0" ];
     };
 
-    networking.firewall.trustedInterfaces = [ "wg0" ];
-
-    networking.wg-quick.interfaces = {
-      wg0 = {
-        address = [ "${hosts."${config.networking.hostName}".ipv4}/24" ];
-        listenPort = cfg.port;
-        privateKeyFile = config.age.secrets.wg.path;
-
-        # TODO: review these once i get good with iptables
-        postUp = lib.mkIf (!cfg.isClient) ''
-          ${pkgs.iptables}/bin/iptables -A FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${
-            hosts."${config.networking.hostName}".ipv4
-          }/24 -o enp2s0 -j MASQUERADE
-        '';
-
-        preDown = lib.mkIf (!cfg.isClient) ''
-          ${pkgs.iptables}/bin/iptables -D FORWARD -i wg0 -j ACCEPT
-          ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${
-            hosts."${config.networking.hostName}".ipv4
-          }/24 -o enp2s0 -j MASQUERADE
-        '';
-
-        peers = lib.mkMerge [
-          (lib.mkIf cfg.isClient [{
-            publicKey = hosts."${alloy.wireguard-server.hostname}".publicKey;
-            endpoint = "93.125.3.204:${toString cfg.port}";
-            persistentKeepalive = 25;
-            allowedIPs =
-              [ "${hosts."${alloy.wireguard-server.hostname}".ipv4}/24" ];
-          }])
-          (lib.mkIf (!cfg.isClient) (alloy.wireguard-client.forEach (host: {
-            publicKey = hosts."${host.hostname}".publicKey;
-            allowedIPs = [ "${hosts."${host.hostname}".ipv4}/32" ];
-          })))
-        ];
+    # TODO: restart trigger
+    services.dnsmasq = lib.mkIf (!cfg.isClient) {
+      enable = true;
+      settings = {
+        interface = "wg0";
+        no-hosts = true;
+        addn-hosts = "${hosts-file}";
       };
+    };
+
+    networking.wg-quick.interfaces.${cfg.interface} = {
+      address = [ "${hosts."${config.networking.hostName}".ipv4}/24" ];
+      listenPort = cfg.port;
+      privateKeyFile = config.age.secrets.wg.path;
+
+      # TODO: review these once i get good with iptables
+      postUp = lib.mkMerge [
+        (lib.mkIf (!cfg.isClient) ''
+          ${pkgs.iptables}/bin/iptables -A FORWARD -i ${cfg.interface} -j ACCEPT
+          ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${server.ipv4}/24 -o enp2s0 -j MASQUERADE
+        '')
+        ''
+          echo 'nameserver ${server.ipv4}' \
+            | ${lib.getExe config.networking.resolvconf.package} -a ${cfg.interface}
+        ''
+      ];
+
+      preDown = lib.mkMerge [(lib.mkIf (!cfg.isClient) ''
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i ${cfg.interface} -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${server.ipv4}/24 -o enp2s0 -j MASQUERADE
+      '')
+      ''
+        ${lib.getExe config.networking.resolvconf.package} -d ${cfg.interface} -f
+      ''];
+
+      peers = lib.mkMerge [
+        (lib.mkIf cfg.isClient [{
+          publicKey = server.publicKey;
+          endpoint = "93.125.3.204:${toString cfg.port}";
+          persistentKeepalive = 25;
+          allowedIPs = [ "${server.ipv4}/24" ];
+        }])
+        (lib.mkIf (!cfg.isClient) (alloy.wireguard-client.forEach (host: {
+          publicKey = hosts."${host.hostname}".publicKey;
+          allowedIPs = [ "${hosts."${host.hostname}".ipv4}/32" ];
+        })))
+      ];
     };
   };
 }
