84b7b56825680ab9cbdcf47d3f55b34dc1635044
Head Gardener <trashbin2019np@gmail.com>
2024-11-15 01:01:21 +0300

    feat(neovim): go gp keymap

--- dots/nvim/ftplugin/go.lua
+++ dots/nvim/ftplugin/go.lua
@@ -2,3 +2,18 @@ vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = false
+
+vim.keymap.set('n', 'gp', function()
+  local current_file = vim.fn.expand("%")
+
+  if string.match(current_file, "_test%.go$") then
+    vim.cmd("e " .. string.gsub(current_file, "_test", ""))
+  else
+    vim.cmd("e " .. string.gsub(current_file, ".go", "_test.go"))
+  end
+end, {
+  noremap = true,
+  buffer = true,
+  -- silent = true,
+  desc = 'Go to pair'
+})
