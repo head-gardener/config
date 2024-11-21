fe2ce9527c5e1335655ec07f6bbd87c44c3189fd
Head Gardener <trashbin2019np@gmail.com>
2024-11-21 20:13:38 +0300

    fix(terraform): don't store secret ids in state

--- terraform/vault/approle.tf
+++ terraform/vault/approle.tf
@@ -3,7 +3,8 @@ resource "vault_auth_backend" "approle" {
 }
 
 locals {
-  machines = {
+  machine = {
+    blueberry = [ ]
     elderberry = [ vault_policy.service_user["sing-box"].name ]
     ambrosia = [
       vault_policy.service_user["sing-box"].name,
@@ -27,29 +28,16 @@ resource "vault_approle_auth_backend_role" service {
   token_type      = "service"
 }
 
-resource "vault_approle_auth_backend_role_secret_id" service {
-  for_each  = local.service
-  backend   = vault_auth_backend.approle.path
-  role_name = vault_approle_auth_backend_role.service[each.key].role_name
-}
-
 output "service_role_id" {
   value = {
     for k, v in local.service : k => vault_approle_auth_backend_role.service[k].role_id
   }
 }
 
-output "service_secret_id" {
-  value = {
-    for k, v in local.service : k => vault_approle_auth_backend_role_secret_id.service[k].secret_id
-  }
-  sensitive = true
-}
-
 # machines
 
 resource "vault_approle_auth_backend_role" machine {
-  for_each        = local.machines
+  for_each        = local.machine
   backend         = vault_auth_backend.approle.path
   role_name       = each.key
   token_policies  = each.value
@@ -57,21 +45,8 @@ resource "vault_approle_auth_backend_role" machine {
   token_type      = "batch"
 }
 
-resource "vault_approle_auth_backend_role_secret_id" machine {
-  for_each  = local.machines
-  backend   = vault_auth_backend.approle.path
-  role_name = vault_approle_auth_backend_role.machine[each.key].role_name
-}
-
 output "machine_role_id" {
   value = {
-    for k, v in local.machines : k => vault_approle_auth_backend_role.machine[k].role_id
+    for k, v in local.machine : k => vault_approle_auth_backend_role.machine[k].role_id
   }
-}
-
-output "machine_secret_id" {
-  value = {
-    for k, v in local.machines : k => vault_approle_auth_backend_role_secret_id.machine[k].secret_id
-  }
-  sensitive = true
 }
