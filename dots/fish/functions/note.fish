8ec846685fe1a6fa096252dcfcd08363d10a898d
Head Gardener <trashbin2019np@gmail.com>
2024-12-25 03:45:54 +0300

    feat(fish): improved note function

--- dots/fish/functions/note.fish
+++ dots/fish/functions/note.fish
@@ -1,17 +1,29 @@
 function note -d "Opens an unmanaged note with the given topic" -a topic
+  set basedir "$HOME/notes"
+
+  function list -a basedir
+    find "$basedir" -type f -name '*.norg' | sed "s|$basedir/\(.*\)\.norg|\1|"
+  end
+
   switch $topic
     case --list
-      ls "$HOME/notes/unmanaged" | sed -n '/.norg/p' | sed 's/.norg//'
+      list "$basedir"
       return 0
     case --select
-      set topic (ls "$HOME/notes/unmanaged" | sed -n '/.norg/p' | sed 's/.norg//' | dmenu)
+      set topic "$(list "$basedir" | dmenu)"
       test -n "$topic" || return 1
   end
 
-  pushd "$HOME/notes/unmanaged"
+  pushd "$basedir"
 
-  set path "$HOME/notes/unmanaged/$topic.norg"
-  set capitalized (echo "$topic" | sed -e 's/\b\(.\)/\u\1/g')
+  # move to unmanaged by default
+  if test ! -e "$basedir/$topic.norg" && not string match -q "*/*" "$topic"
+    set topic "unmanaged/$topic"
+  end
+
+  set path "$basedir/$topic.norg"
+  set title "$(echo "$topic" | sed 's/.*\///')"
+  set capitalized (echo "$title" | sed -e 's/\b\(.\)/\u\1/g')
   set args (test -e "$path" || echo "+\"normal i* $capitalized\"")
   sh -c "nvim $path $args"
 
