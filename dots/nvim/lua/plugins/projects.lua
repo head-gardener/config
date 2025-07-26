464ad97d356bc0d1778a050dbee6ee850f354906
Nikita Puchynski <49650767+head-gardener@users.noreply.github.com>
2025-07-26 18:27:25 +0300

    chore(neovim): update, some fixes (#25)

--- dots/nvim/lua/plugins/projects.lua
+++ dots/nvim/lua/plugins/projects.lua
@@ -1,5 +1,22 @@
 return {
   {
+    "juacker/git-link.nvim",
+    keys = {
+      {
+        "<leader>gu",
+        function() require("git-link.main").copy_line_url() end,
+        desc = "Copy code link to clipboard",
+        mode = { "n", "x" }
+      },
+      {
+        "<leader>go",
+        function() require("git-link.main").open_line_url() end,
+        desc = "Open code link in browser",
+        mode = { "n", "x" }
+      },
+    },
+  },
+  {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
     dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
