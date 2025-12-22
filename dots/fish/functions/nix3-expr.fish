e783f3c3a850e01344f2ddfead33e1f7665f14e6
Head Gardener <trashbin2019np@gmail.com>
2025-12-23 00:03:47 +0300

    feat(fish): better nix3-expr

---
+++ dots/fish/functions/nix3-expr.fish
@@ -0,0 +1,32 @@
+function nix3-expr \
+  -a op \
+  -a expr \
+  -d 'Build/shell/develop a nix expression with automatic flake imports.'
+
+  # flatten a flake to make it structurally resemble cli flake urls.
+  set -l shorten_func '
+    shorten = f:
+      let
+        legacyPackages = if f ? "legacyPackages" then f.legacyPackages.${builtins.currentSystem} else {};
+        packages = if f ? "packages" then f.packages.${builtins.currentSystem} else {};
+        devShells = if f ? "devShells" then f.devShells.${builtins.currentSystem} else {};
+      in f
+        // legacyPackages
+        // packages
+        // {
+          inherit legacyPackages packages devShells;
+        }'
+
+  nix $op --impure --expr \
+    "let
+      $shorten_func;
+      $(for f in $(nix registry list);
+          echo -n '"'
+          echo -n "$f" | sed -nr 's/.*flake:(.*)\s.*/\1/p'
+          echo -n '"'
+          echo " = shorten (builtins.getFlake \"$(echo "$f" | cut -d' ' -f3)\");"
+        end)
+      dot = shorten (builtins.getFlake \"s\");
+    in $expr" \
+    $argv[3..]
+end
--- dots/fish/functions/nix3-shell-expr.fish
+++
@@ -1,8 +0,0 @@
-function nix3-shell-expr \
-  -a expr \
-  -d 'Open nix3 shell from an expression rather than a url'
-
-  nix shell --impure --expr \
-    "let s = (builtins.getFlake \"s\").legacyPackages.\${builtins.currentSystem}; in $expr" \
-    $argv[2..]
-end
