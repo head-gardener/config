3854dd99cf9b2543f959853c8973bde8459fe8b1
Head Gardener <trashbin2019np@gmail.com>
2026-01-11 10:53:29 +0300

    feat(terraform) flake init

---
+++ terraform/flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    parent.url = "git+path:/..";
+  };
+
+  outputs =
+    inputs:
+    with inputs;
+    parent.inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        { inputs' }:
+        {
+          packages = {
+            digitaloceanUbuntu = inputs'.parent.packages.cloud-vm.override {
+              imgHash = "sha256-uVLOFcwDNdoNBVP2pJLvhJdE7kp5OV5FiHSUHLhTa9M=";
+              imgSize = "25G";
+              extraUserData = ''
+                runcmd:
+                  - |
+                    set -e
+                    mount -t 9p host1 /mnt1 --mkdir
+                    mount -t 9p host2 /mnt2 --mkdir
+                    mount -t 9p host3 /mnt3 --mkdir
+                    INIT_OPTS="-x" CLOUD_CLEAR_SEMAPHORE=auto TMPFS_BALANCE=1 \
+                      /mnt1/ext2btrfs -x || exit 0
+                    btrfs filesystem show /
+                    mkdir /tmp/store
+                    tar xzf /mnt2/eval_store.tar.gz -C /tmp/store
+                    cat /mnt3/nixos-infect \
+                      | PROVIDER=digitalocean NIXOS_FLAKE="github:head-gardener/config" \
+                        HOSTNAME="elderberry" EVAL_STORE="/tmp/store" NO_REBOOT="1" \
+                        bash -x
+                    btrfs filesystem label / nixos
+                    sync
+                    reboot -f
+              '';
+            };
+          };
+        };
+    };
+}
