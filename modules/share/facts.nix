ffd5a30a705ddc41fee4d4325a7d5ca395370ce3
Head Gardener <trashbin2019np@gmail.com>
2026-01-11 12:09:06 +0300

    feat(gather-facts): add testing and a script wrapper

--- modules/share/facts.nix
+++ modules/share/facts.nix
@@ -1,15 +1,93 @@
-{ lib, ... }:
-let
-  inherit (lib) mkOption types;
-in
 {
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  cfg = config.personal.facts;
+  exists = builtins.pathExists cfg.factsFile;
 
-  options.personal.facts = mkOption {
-    type = types.raw;
-    default = { };
-    description = lib.mdDoc ''
-      Facts about a host
+  perlEnv = pkgs.perl;
+  pkg = pkgs.writeShellApplication {
+    name = "facts";
+    runtimeInputs = with pkgs; [ iproute2 ];
+    text = ''
+      "${lib.getExe' perlEnv "perl"}" "${inputs.self}/scripts/gather-facts.pl" \
+        | ${lib.getExe pkgs.jq} -S .
     '';
   };
+in
+{
+  options.personal.facts = {
+    enable = lib.mkEnableOption "facts";
 
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = pkg;
+      description = "Wrapped gather-facts.pl script.";
+    };
+
+    factsFile = lib.mkOption {
+      type = lib.types.path;
+      default = "${inputs.self}/hosts/${config.networking.hostName}/facts.json";
+      description = "Path to read-only facts file.";
+    };
+
+    facts = lib.mkOption {
+      type = lib.types.nullOr lib.types.attrs;
+      readOnly = true;
+      description = "Non-null if facts are available.";
+    };
+  };
+
+  config = lib.mkMerge [
+    {
+      personal.facts.facts = lib.mkMerge [
+        (lib.mkIf (exists && cfg.enable) (builtins.fromJSON (builtins.readFile cfg.factsFile)))
+        (lib.mkIf (!(exists && cfg.enable)) (
+          lib.warn ''
+            Facts file ${cfg.factsFile} doesn't exit or isn't enabled!
+          '' null
+        ))
+      ];
+    }
+    (lib.mkIf (cfg.enable && exists) {
+      systemd.services.check-facts = {
+        description = "Check facts for consistency";
+        after = [ "multi-user.target" ];
+        wantedBy = [ "multi-user.target" ];
+
+        path = [
+          pkgs.diffutils
+        ];
+
+        script = ''
+          set +e
+          (
+            set -eo pipefail
+            test -f "${cfg.factsFile}"
+            diff "${cfg.factsFile}" <("${lib.getExe cfg.package}")
+          )
+          rc="$?"
+          if [ "$rc" -ne 0 ]; then
+            echo "Warning: facts changed!"
+            echo -n 'Tried "diff ${cfg.factsFile} <(${lib.getExe cfg.package})"'
+            exit 1
+          else
+            echo "Facts are up-to-date."
+            exit 0
+          fi
+        '';
+
+        serviceConfig = {
+          Type = "oneshot";
+          Restart = "no";
+          SuccessExitStatus = "0 1";
+          RemainAfterExit = true;
+        };
+      };
+    })
+  ];
 }
