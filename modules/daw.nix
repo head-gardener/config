7cb5635bf7173565ae5d2f46c4f0226e423d5cb8
Head Gardener <trashbin2019np@gmail.com>
2026-01-23 10:01:39 +0300

    feat(daw): sfz

--- modules/daw.nix
+++ modules/daw.nix
@@ -1,13 +1,43 @@
 { pkgs, inputs, ... }:
 let
-  pluginPaths =
-    [ "/lib/vst" "/lib/vst3" "/lib/lv2" "/lib/lxvst" "/lib/ladspa" ];
-in {
+  pluginPaths = [
+    "/lib/vst"
+    "/lib/vst3"
+    "/lib/lv2"
+    "/lib/lxvst"
+    "/lib/ladspa"
+  ];
+
+  repoToSFZ =
+    args:
+    pkgs.stdenvNoCC.mkDerivation (
+      let
+        name = "${args.owner}-${args.repo}";
+      in
+      {
+        pname = name;
+        version = args.rev;
+
+        src = pkgs.fetchFromGitHub args;
+
+        installPhase = ''
+          mkdir -p "$out/opt/sfz/$name"
+          cp -r . "$out/opt/sfz/$name/"
+        '';
+      }
+    );
+in
+{
   imports = [ inputs.musnix.nixosModules.musnix ];
 
-  nixpkgs.allowUnfreeByName = [ "reaper" "vst2-sdk" ];
+  nixpkgs.allowUnfreeByName = [
+    "reaper"
+    "vst2-sdk"
+  ];
 
-  environment.pathsToLink = pluginPaths;
+  environment.pathsToLink = pluginPaths ++ [
+    "/opt/sfz"
+  ];
 
   musnix.enable = true;
   users.users.hunter.extraGroups = [ "audio" ];
@@ -19,6 +49,26 @@ in {
     guitarix
     hydrogen
     reaper
+    inputs.unstable.legacyPackages.${pkgs.stdenv.hostPlatform.system}.sfizz-ui
+
+    (repoToSFZ {
+      owner = "sfzinstruments";
+      repo = "SplendidGrandPiano";
+      rev = "1c595d827b7fd7f0be3134aaed0f90b3662a09cf";
+      hash = "sha256-wVP0IZEDlnXyxzrf+9tN/hPSV5jKg4CS5tJC/N2XX7Y=";
+    })
+    (repoToSFZ {
+      owner = "sfzinstruments";
+      repo = "karoryfer.emilyguitar";
+      rev = "b4920dc662fd9cad6dcaccdeecffdd91c8725d8c";
+      hash = "sha256-oFxC7dbVqjkmdYvR7qnfmWTvtlC8mjPNUggn6MYXVTI=";
+    })
+    (repoToSFZ {
+      owner = "sfzinstruments";
+      repo = "karoryfer.gogodze-phu-vol-ii";
+      rev = "69a0274cdc39c99c3098cde1bc3789690de86d62";
+      hash = "sha256-27ZoDI3hqPp25H0XzQl6atnwfEKBSrx8YI1mFGtEGAI=";
+    })
 
     aether-lv2
     airwindows
