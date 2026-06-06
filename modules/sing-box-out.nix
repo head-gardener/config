9a263106f39a553d63b1c63adfb090890a77f495
Head Gardener <trashbin2019np@gmail.com>
2026-06-06 17:18:37 +0300

    fix(sing-box-out): vless, reality. ! no client config yet, i don't have vault for proper secrets

--- modules/sing-box-out.nix
+++ modules/sing-box-out.nix
@@ -15,9 +15,21 @@
   };
 
   config = {
-    personal.va.templates.vmess-uuid = lib.mkIf (!config.personal.sing-box.vaultless) {
-      path = "services/sing-box/vmess-uuid";
+    personal.va.templates.vless-uuid = lib.mkIf (!config.personal.sing-box.vaultless) {
+      path = "services/sing-box/vless-uuid";
       field = "uuid";
+      for = "sing-box.service";
+    };
+
+    personal.va.templates.reality-privkey = lib.mkIf (!config.personal.sing-box.vaultless) {
+      path = "services/sing-box/reality-privkey";
+      field = "privkey";
+      for = "sing-box.service";
+    };
+
+    personal.va.templates.reality-short-id = lib.mkIf (!config.personal.sing-box.vaultless) {
+      path = "services/sing-box/reality-short-id";
+      field = "shortid";
       for = "sing-box.service";
     };
 
@@ -29,25 +41,49 @@
         inbounds = [
           {
             listen = "::";
-            listen_port = 19555;
-            type = "vmess";
-            tag = "vmess-in";
+            listen_port = 443;
+            type = "vless";
+            tag = "vless-in";
             users = [
               {
                 name = "default";
                 uuid._secret = lib.mkMerge [
                   (lib.mkIf
                     (!config.personal.sing-box.vaultless)
-                    config.personal.va.templates.vmess-uuid.destination)
+                    config.personal.va.templates.vless-uuid.destination)
                   (lib.mkIf
                     config.personal.sing-box.vaultless
-                    "/run/secret/vmess-uuid")
+                    "/run/secret/vless-uuid")
                 ];
-                alterID = 0;
+                flow = "xtls-rprx-vision";
               }
             ];
             tls = {
-              enabled = false;
+              enabled = true;
+              server_name = "yandex.ru";
+              reality = {
+                enabled = true;
+                handshake = {
+                  server = "yandex.ru";
+                  server_port = 443;
+                };
+                private_key._secret = lib.mkMerge [
+                  (lib.mkIf
+                    (!config.personal.sing-box.vaultless)
+                    config.personal.va.templates.reality-privkey.destination)
+                  (lib.mkIf
+                    config.personal.sing-box.vaultless
+                    "/run/secret/reality-privkey")
+                ];
+                short_id._secret = lib.mkMerge [
+                  (lib.mkIf
+                    (!config.personal.sing-box.vaultless)
+                    config.personal.va.templates.reality-short-id.destination)
+                  (lib.mkIf
+                    config.personal.sing-box.vaultless
+                    "/run/secret/reality-short-id")
+                ];
+              };
             };
           }
         ];
@@ -60,11 +96,16 @@
         route = {
           final = "direct";
           auto_detect_interface = true;
-          rules = [ ];
+          rules = [
+            {
+              ip_is_private = true;
+              outbound = "direct";
+            }
+          ];
         };
       };
     };
 
-    networking.firewall.allowedTCPPorts = [ 19555 ];
+    networking.firewall.allowedTCPPorts = [ 443 ];
   };
 }
