8a3a05b25998a22bc239db5d2290743eb008258b
Head Gardener <trashbin2019np@gmail.com>
2026-06-27 19:29:36 +0300

    fix(checks): set type for checks options

--- modules/share/checks.nix
+++ modules/share/checks.nix
@@ -1,15 +1,23 @@
 { lib, ... }:
-let
-  inherit (lib) mkOption types;
-in
 {
-
-  options.personal.checks = mkOption {
-    type = types.raw;
+  options.personal.checks = lib.mkOption {
+    type = lib.types.attrsOf (lib.types.submodule {
+      options = {
+        modules = lib.mkOption {
+          type = lib.types.listOf lib.types.deferredModule;
+          default = [];
+        };
+        script = lib.mkOption {
+          type = lib.types.lines;
+          default = ''
+            machine.wait_for_unit("multi-user.target")
+          '';
+        };
+      };
+    });
     default = { };
     description = lib.mdDoc ''
       Checks to perform
     '';
   };
-
 }
