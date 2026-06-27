9782ec30d814e30558e6a27cc6c03a4c4a1aab05
Head Gardener <trashbin2019np@gmail.com>
2026-06-28 01:14:35 +0300

    feat(impermanence): grand rework, check

--- hosts/cherry/configuration.nix
+++ hosts/cherry/configuration.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     (inputs.self.lib.mkKeys inputs.self "hunter")
+    inputs.self.nixosModules.impermanence
     inputs.self.nixosModules.zram
   ];
 
@@ -14,52 +15,6 @@
   };
   boot.loader.efi = {
     canTouchEfiVariables = true;
-  };
-
-  boot.initrd.postDeviceCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount ${config.fileSystems."/".device} /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
-
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
-
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
-
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
-  '';
-
-  environment.persistence."/persistent" = {
-    hideMounts = true;
-    directories = [
-      "/etc/ssh"
-    ];
-    users.hunter = {
-      directories = [
-        ".local/share/fish"
-        ".local/share/kitty-ssh-kitten"
-        ".terminfo"
-      ];
-    };
-    files = [ ];
-  };
-
-  fileSystems."/etc/ssh" = {
-    depends = [ "/persist" ];
-    neededForBoot = true;
-    fsType = "none";
   };
 
   networking.networkmanager.enable = false;
---
+++ modules/impermanence.nix
@@ -0,0 +1,168 @@
+{ config, inputs, utils, ... }:
+
+{
+  personal.checks.impermanence = {
+    modules = [
+      inputs.self.nixosModules.impermanence
+      (
+        { pkgs, config, lib, ... }:
+        {
+          environment.systemPackages = [ pkgs.btrfs-progs ];
+          services.openssh.enable = lib.mkForce false;
+
+          boot.initrd.systemd.extraBin = {
+            btrfs = "${pkgs.btrfs-progs}/bin/btrfs";
+            grep = "${pkgs.gnugrep}/bin/grep";
+          };
+
+          # this is SLOP
+          virtualisation.fileSystems."/" = {
+            options = [ "subvol=root" ];
+          };
+
+          virtualisation.fileSystems."/persistent" = {
+            device = config.fileSystems."/".device;
+            neededForBoot = true;
+            fsType = "btrfs";
+            options = [ "subvol=persistent" ];
+          };
+
+          boot.initrd.systemd.services."btrfs-subvols" = let
+            root = config.fileSystems."/".device;
+          in {
+            unitConfig.DefaultDependencies = false;
+            serviceConfig.Type = "oneshot";
+            requiredBy = [ "initrd.target" ];
+            before = [ "wipe-file-systems.service" ];
+
+            requires = [
+              "${utils.escapeSystemdPath root}.device"
+              "modprobe@btrfs.service"
+            ];
+            after = [
+              "${utils.escapeSystemdPath root}.device"
+              "local-fs-pre.target"
+              "systemd-makefs@${utils.escapeSystemdPath root}.service"
+              "modprobe@btrfs.service"
+            ];
+
+            script = ''
+              set -x
+
+              udevadm settle
+              mount /dev/vda /mnt --mkdir
+              btrfs subvolume list /mnt | grep persistent && exit 0
+              btrfs subvolume create /mnt/root
+              btrfs subvolume create /mnt/persistent
+              btrfs subvolume create /mnt/old_roots
+              umount /mnt
+            '';
+          };
+        }
+      )
+    ];
+    script = ''
+      machine.wait_for_unit("multi-user.target")
+      machine.succeed(
+          "echo 'stale-data' > /will-be-wiped && "
+          "btrfs subvolume create /dead-subvol && "
+          "touch /dead-subvol/nested-file &&"
+          "touch /var/lib/nixos/persistent"
+      )
+      machine.succeed("test -f /persistent/var/lib/nixos/persistent")
+
+      # check rotation on reboot
+      machine.shutdown()
+      machine.start()
+      machine.wait_for_unit("multi-user.target")
+      machine.fail("test -f /will-be-wiped")
+      machine.fail("test -d /dead-subvol")
+      machine.succeed("test -f /var/lib/nixos/persistent")
+
+      def mount_top():
+        machine.succeed(
+            "mkdir /tmp/top && "
+            "mount -o subvolid=5 /dev/vda /tmp/top"
+        )
+        entries = machine.succeed("ls /tmp/top/old_roots").strip().split()
+        assert len(entries) > 0, "no old_roots entries!"
+        # ls is alpabetical, newest date is always last
+        return entries
+
+      roots = mount_top()
+      old_root = roots[-1]
+      machine.succeed(f"test -f /tmp/top/old_roots/{old_root}/will-be-wiped")
+      machine.succeed(f"test -f /tmp/top/old_roots/{old_root}/dead-subvol/nested-file")
+      machine.succeed(f"touch -m -d '31 days ago' /tmp/top/old_roots/{old_root}")
+
+      # check cleanup
+      machine.shutdown()
+      machine.start()
+      machine.wait_for_unit("multi-user.target")
+      mount_top()
+      machine.fail(f"test -d /tmp/top/old_roots/{old_root}")
+    '';
+  };
+
+  boot.initrd.systemd = {
+    services.wipe-file-systems = {
+      unitConfig.DefaultDependencies = false;
+      serviceConfig.Type = "oneshot";
+      wantedBy = [ "initrd.target" ];
+      before = [ "sysroot.mount" ];
+
+      requires = [ "${utils.escapeSystemdPath config.fileSystems."/".device}.device" ];
+      after = [
+        "${utils.escapeSystemdPath config.fileSystems."/".device}.device"
+        "local-fs-pre.target"
+        "systemd-makefs@${utils.escapeSystemdPath config.fileSystems."/".device}.service"
+      ];
+
+      script = ''
+        set -x
+
+        mkdir /btrfs_tmp
+        mount '${config.fileSystems."/".device}' /btrfs_tmp
+        if [[ -e /btrfs_tmp/root ]]; then
+          mkdir -p /btrfs_tmp/old_roots
+          timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+          mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+        fi
+
+        for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+          btrfs subvolume delete --recursive "$i"
+        done
+
+        btrfs subvolume create /btrfs_tmp/root
+        umount /btrfs_tmp
+      '';
+    };
+  };
+
+  environment.persistence."/persistent" = {
+    hideMounts = true;
+    directories = [
+      "/etc/ssh"
+      "/var/lib/nixos"
+    ];
+    users.hunter = {
+      directories = [
+        ".local/share/fish"
+        ".local/share/kitty-ssh-kitten"
+        ".terminfo"
+      ];
+    };
+    files = [ ];
+  };
+
+  # TODO: i don't remember why i put this in. there's no file system there,
+  # imperm doesn't create filesystems. this probably tricks nix into something
+  # having to do with neededForBoot for mount ordering, but i don't understand
+  # how exactly this works. investigate once i have access to rebooting this
+  # thing.
+  fileSystems."/etc/ssh" = {
+    depends = [ "/persist" ];
+    neededForBoot = true;
+    fsType = "none";
+  };
+}
