c3116563406016d2780fdd6596cba303f47adbc9
Head Gardener <trashbin2019np@gmail.com>
2026-07-12 16:15:07 +0300

    fix(bluetooth): multipoint

--- modules/desktop/bluetooth.nix
+++ modules/desktop/bluetooth.nix
@@ -2,6 +2,14 @@
 {
   hardware.bluetooth.enable = true;
 
+  hardware.bluetooth.settings = {
+    General = {
+      ControllerMode = "dual";
+      MultiProfile = "multiple";
+      FastConnectable = true;
+    };
+  };
+
   environment.systemPackages = with pkgs; [
     blueberry
     pavucontrol
