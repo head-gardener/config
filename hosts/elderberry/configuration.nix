088adc6d164eb1e4f127856c0277f9836f7c3f7e
Head Gardener <trashbin2019np@gmail.com>
2026-08-10 00:01:14 +0300

    fix(elderberry): kexec-installer, some cleanup

--- hosts/elderberry/configuration.nix
+++ hosts/elderberry/configuration.nix
@@ -1,45 +1,27 @@
-{ inputs, pkgs, lib, ... }:
+{ inputs, modulesPath, ... }:
 
 {
   imports = [
     inputs.self.nixosModules.providers-digitalocean
     (inputs.self.lib.mkKeys inputs.self "hunter")
     inputs.self.nixosModules.zram
+    inputs.self.nixosModules.kexec-installer
   ];
 
-  fileSystems."/" =
-    { label = "nixos";
-      fsType = "btrfs";
-      options = [
-        "subvolid=5"
-        "x-systemd.growfs"
-      ];
-    };
+  personal.kexecInstaller = {
+    extraImports = [
+      inputs.self.nixosModules.providers-digitalocean
+      "${modulesPath}/virtualisation/digital-ocean-config.nix"
+    ];
+    flavor = "1g";
+    unwrap = false;
+  };
 
   swapDevices = [{
     device = "/swapfile";
     size = 2048;
   }];
 
-  environment.systemPackages = with pkgs; [
-    fish
-  ];
-
   networking.firewall.enable = true;
-  networking.firewall.allowedTCPPorts = [ ];
-  networking.firewall.allowedUDPPorts = [ ];
   networking.firewall.allowPing = false;
-
-  programs = {
-    atop = {
-      enable = true;
-      atopService.enable = true;
-      atopRotateTimer.enable = true;
-      netatop.enable = true;
-      settings = {
-        interval = 5;
-        flags = "1fA";
-      };
-    };
-  };
 }
