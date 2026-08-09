5869163754226631cf8cf1b05c018a8b56569003
Head Gardener <trashbin2019np@gmail.com>
2026-08-09 23:24:06 +0300

    feat(foxgrape): installer

--- hosts/foxgrape/configuration.nix
+++ hosts/foxgrape/configuration.nix
@@ -8,8 +8,16 @@
   imports = [
     ./hardware-configuration.nix
     (inputs.self.lib.mkKeys inputs.self "hunter")
+    inputs.self.nixosModules.kexec-installer
     inputs.self.nixosModules.providers-datalix
   ];
+
+  personal.kexecInstaller = {
+    firmware = "uefi";
+    extraImports = [
+      inputs.self.nixosModules.providers-datalix
+    ];
+  };
 
   networking.firewall.enable = true;
   networking.firewall.allowPing = false;
